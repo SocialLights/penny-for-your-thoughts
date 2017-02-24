f456692b8d47ac63ed2c498eeca54edbd705c4f0
mnewsome <malcolm.newsome@gmail.com>
2017-02-24 17:35:45 -0600

    adds ability to unlock multiple posts
    Right now has a temporary form to enter the number of the posts that are
    to be unlock.  This will be replaced in the future.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,11 @@ class PostsController < ApplicationController
     Post.create!(post_params)
   end
 
+  def unlock
+    Post.unlock(params[:number_to_unlock].to_i)
+    redirect_to 'posts/index'
+  end
+
   private
 
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,18 @@
 class Post < ApplicationRecord
+
+  scope :locked, -> (number_of_posts) { where(is_locked: true).order(:created_at).first(number_of_posts) }
+
   def class_name
     self.is_locked ? "locked" : "unlocked"
   end
+
+  def self.unlock(number_of_posts)
+    locked_posts = Post.locked(number_of_posts)
+
+    locked_posts.each do |post|
+      post.is_locked = false
+      post.save
+    end
+  end
+
 end
